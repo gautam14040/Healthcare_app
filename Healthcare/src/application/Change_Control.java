9f47a044234c6fa9ec0b6708846423188600743e
gautam <gautam14040@iiitd.ac.in>
2016-04-28 12:12:09 +0530

    Update 1.01

---
+++ Healthcare/src/application/Change_Control.java
@@ -0,0 +1,115 @@
+package application;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
+
+public class Change_Control {
+
+	public static Employee e1;
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<Integer> slot2_change_schedule;
+
+    @FXML
+    private Text error_change_schedule;
+
+    @FXML
+    private Button back_change_schedule;
+
+    @FXML
+    private Button logout_change_schedule;
+
+    @FXML
+    private ComboBox<Integer> slot3_change_schedule;
+
+    @FXML
+    private Button done_change_schedule;
+
+    @FXML
+    private ComboBox<Integer> slot1_change_schedule;
+
+    @FXML
+    void enter(ActionEvent event) {
+    	if(slot1_change_schedule.getValue()==null || slot3_change_schedule.getValue()==null || slot2_change_schedule.getValue()==null ||slot1_change_schedule.getValue()==slot2_change_schedule.getValue()||  
+    			slot2_change_schedule.getValue()==slot3_change_schedule.getValue() || slot1_change_schedule.getValue()==slot3_change_schedule.getValue())
+    		{
+    		error_change_schedule.setText("Please choose the correct slots");
+    			return;
+    		}
+    	System.out.println("hereupdea");
+    	int slot1=slot1_change_schedule.getValue();
+    	int slot2=slot2_change_schedule.getValue();
+    	int slot3=slot3_change_schedule.getValue();
+    	String avail="111";
+    	String no="000";
+    	Connection connection = null;
+        Statement statement = null; 
+    	ResultSet rs = null;
+    	String slot=new String();
+    	for(int i=1;i<=6;i++)
+    	{
+    		if(i==slot1 || i==slot2 || i==slot3 )
+    		{
+    			slot=slot+avail;
+    		}
+    		else
+    		{
+    			slot=slot+no;
+    		}
+    	}
+    	
+    	String query="UPDATE Employee SET slot="+slot+" WHERE emp_id="+e1.emp_id;
+    	try {           
+    	       connection = JDBCMySQLConnection.getConnection();
+    	       statement = connection.createStatement();
+    	        statement.executeUpdate(query);
+    	        System.out.println("Connection successful");
+    	}
+    	catch (SQLException e) {
+             e.printStackTrace();
+          } finally {
+              if (connection != null) {
+                  try {
+                	  done_change_schedule.setDisable(true);
+                    connection.close();
+                  } catch (SQLException e) {
+                    e.printStackTrace();
+                  }
+              }}
+    }
+    @FXML
+    void initialize() {
+        assert slot2_change_schedule != null : "fx:id=\"slot2_change_schedule\" was not injected: check your FXML file 'Change_schedule.fxml'.";
+        assert error_change_schedule != null : "fx:id=\"error_change_schedule\" was not injected: check your FXML file 'Change_schedule.fxml'.";
+        assert back_change_schedule != null : "fx:id=\"back_change_schedule\" was not injected: check your FXML file 'Change_schedule.fxml'.";
+        assert logout_change_schedule != null : "fx:id=\"logout_change_schedule\" was not injected: check your FXML file 'Change_schedule.fxml'.";
+        assert slot3_change_schedule != null : "fx:id=\"slot3_change_schedule\" was not injected: check your FXML file 'Change_schedule.fxml'.";
+        assert done_change_schedule != null : "fx:id=\"done_change_schedule\" was not injected: check your FXML file 'Change_schedule.fxml'.";
+        assert slot1_change_schedule != null : "fx:id=\"slot1_change_schedule\" was not injected: check your FXML file 'Change_schedule.fxml'.";
+        ArrayList<Integer> x1 = new ArrayList<Integer>();
+        for(int i=0 ; i <= 5; i++)
+        	x1.add(i+1);
+        error_change_schedule.setText("");
+        slot1_change_schedule.setItems(FXCollections.observableArrayList(x1));
+        slot2_change_schedule.setItems(FXCollections.observableArrayList(x1));
+        slot3_change_schedule.setItems(FXCollections.observableArrayList(x1));
+
+    }
+}
